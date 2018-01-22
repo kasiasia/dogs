4df92c687ea415b99515fae0029d828973e71805
Kasia Urbaniak <kas.urbaniak@gmail.com>
2018-01-22 22:45:23 +0100

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+const errorMessage = "I didn't find any pictures...:( ";
+const breed = "beagle";
+const url="https://dog.ceo/dog-api/breeds/image/random";
+
+console.log(url);
+
+
+class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      breed: '',
+      items: []
+    };
+  }
+
+  /*this.searchBreed = this.searchBreed.bind(this);*/
+
+  searchBreed(breed) {
+    return !this.state.results[breed];
+  }
+
+
+
+  componentDidMount() {
+    fetch("https://dog.ceo/dog-api/breed/breeds/image/random")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  render() {
+    return (
+        <div className="App">
+           <div className="Container">
+                <div className="App-header">
+                  <h1><span className="HeaderSpan">Dog</span>App</h1>
+                </div>
+                <div className="Search">
+                    <form onSubmit={this.props.onSubmit} id="search" className="SearchForm">
+        				<input type="search" className="SearchInput" placeholder="Breed name" />
+                        <button className="SearchBtn" type="submit">
+                            SEARCH
+                        </button>
+        		    </form>
+
+                </div>
+            </div>
+          </div>
+    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {errorMessage}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default App;
